29071140645de832812de4d0da081de7b52100c9
vstepenko <vstepencko>
2023-10-03 20:05:45 +0300

    init commit

---
+++ chainofresp/src/main/java/ua/edu/duan/gof/Application.java
@@ -0,0 +1,7 @@
+package ua.edu.duan.gof;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ iterator/src/main/java/ua/edu/duan/gof/Application.java
@@ -0,0 +1,8 @@
+package ua.edu.duan.gof;
+
+public class Application {
+    public static void main(String[] args) {
+
+    }
+
+}
